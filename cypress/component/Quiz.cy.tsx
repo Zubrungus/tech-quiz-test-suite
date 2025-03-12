48baddbeed8b706eef919938d291b59e317702a3
Zubrungus <58639394+Zubrungus@users.noreply.github.com>
2025-03-11 21:29:36 -0500

    Finish e2e testing, add comments

--- cypress/component/Quiz.cy.tsx
+++ cypress/component/Quiz.cy.tsx
@@ -1,5 +1,6 @@
 import Quiz from '../../client/src/components/Quiz'
 
+//data for a specific question
 const singleQuestion = [{
     "question": "What is the output of print(2 ** 3)?",
     "answers": [
@@ -10,9 +11,9 @@ const singleQuestion = [{
     ]
 }]
 
-
 describe('<Quiz />', () => {
     beforeEach(() => {
+        //intercept the api request and supply the specific question
         cy.intercept('GET', '/api/questions/random', (req) => {
             req.reply({
                 statusCode: 200,
@@ -21,11 +22,13 @@ describe('<Quiz />', () => {
         }).as('getQuestion');
     });
 
+    //button should contain the text Start Quiz
     it('should have a button with the text Start Quiz', () => {
         cy.mount(<Quiz />);
         cy.get('button').should('contain', 'Start Quiz');
     });
 
+    //count the number of children held by the div that holds the answers, which should be 4
     it('should have four listed answers', () => {
         cy.mount(<Quiz />);
         cy.get('button').click();
--- cypress/e2e/quiz.cy.jsx
+++ cypress/e2e/quiz.cy.jsx
@@ -2,41 +2,63 @@ import QUESTIONS from '../fixtures/questions.json'
 
 describe('Quiz Game Cycle', () => {
     beforeEach(() => {
+        //intercept the api request for random questions and supply 10 ten specific questions
         cy.intercept('GET', '/api/questions/random', (req) => {
             req.reply({
                 statusCode: 200,
                 body: QUESTIONS,
             });
         }).as('getQuestion');
-        cy.visit('/');
+        cy.visit('http://127.0.0.1:3001/');
     });
-
+    
+    //start game button should be visible
     it('should have a button to start the game', () => {
         cy.get('button').should('be.visible');
     });
 
+    //the div that contains the answers should have 4 children, meaning 4 listed answers
     it('should have four listed answers', () => {
         cy.get('button').click();
         cy.wait('@getQuestion');
         cy.get('.mt-3').children().should('have.length', 4);
     });
 
-    it('should show the results screen with a correct score after clicking 10 times', () => {
+    it('should show the results screen with a correct score after clicking the first answer 10 times', () => {
         cy.get('button').click();
         cy.wait('@getQuestion');
-        cy.get('.mt-3').first().first().click();
-        cy.get('.mt-3').first().first().click();
-        cy.get('.mt-3').first().first().click();
-        cy.get('.mt-3').first().first().click();
-        cy.get('.mt-3').first().first().click();
-        cy.get('.mt-3').first().first().click();
-        cy.get('.mt-3').first().first().click();
-        cy.get('.mt-3').first().first().click();
-        cy.get('.mt-3').first().first().click();
-        cy.get('.mt-3').first().first().click();
-
+        //click the first answer all 10 times
+        cy.get('.mt-3').children().first().children().first().click();
+        cy.get('.mt-3').children().first().children().first().click();
+        cy.get('.mt-3').children().first().children().first().click();
+        cy.get('.mt-3').children().first().children().first().click();
+        cy.get('.mt-3').children().first().children().first().click();
+        cy.get('.mt-3').children().first().children().first().click();
+        cy.get('.mt-3').children().first().children().first().click();
+        cy.get('.mt-3').children().first().children().first().click();
+        cy.get('.mt-3').children().first().children().first().click();
+        cy.get('.mt-3').children().first().children().first().click();
+        //given the questions received from fixtures/questions.json, score should always be 2/10 if first answer is always clicked
+        cy.get('.alert-success').should('contain','Your score: 2/10');
     })
 
-
+    it('should restart the game when the Take New Quiz button is clicked', () => {
+        cy.get('button').click();
+        cy.wait('@getQuestion');
+        cy.get('.mt-3').children().first().children().first().click();
+        cy.get('.mt-3').children().first().children().first().click();
+        cy.get('.mt-3').children().first().children().first().click();
+        cy.get('.mt-3').children().first().children().first().click();
+        cy.get('.mt-3').children().first().children().first().click();
+        cy.get('.mt-3').children().first().children().first().click();
+        cy.get('.mt-3').children().first().children().first().click();
+        cy.get('.mt-3').children().first().children().first().click();
+        cy.get('.mt-3').children().first().children().first().click();
+        cy.get('.mt-3').children().first().children().first().click();
+        //after going through the quiz, ensure we are clicking the Take New Quiz button
+        cy.get('.d-inline-block').should('contain', 'Take New Quiz').click();
+        //check if the header contains a question mark, meaning a question is on screen and the game has been restarted
+        cy.get('h2').should('contain', '?')
+    })
 
 });
